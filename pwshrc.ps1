7e5b60b45ae5fb6a9785130df5f522ec5b83c2c0
tianhantang <tianhantang.pd@gmail.com>
2024-03-14 00:36:30 +0900

    Add PowerShell configuration script

---
+++ pwshrc.ps1
@@ -0,0 +1,40 @@
+<# 
+	@brief: 
+	PowerShell profile script that is loaded at start time.
+
+	@details:
+	To use this script,
+	- copy it to the default profile location for the OS,
+	- or create a symlink to this file from the default profile location.
+
+	@details:
+	This script will load external modules.
+
+	@date:
+	- created on 2021-06-08
+	- updated on 2024-03-13
+#>
+
+# Get the execution path
+# ////////////////////////////////////////////////////////////
+$entry_point = Get-ItemProperty $MyInvocation.MyCommand.Path
+if ($null -ne $entry_point.Target) { # It is called via a symbolic link
+	$script_dir = Split-Path $entry_point.Target -Parent
+}
+else {
+	$script_dir = $entry_point.Directory.FullName
+}
+
+# Load workspace settings
+# ////////////////////////////////////////////////////////////
+# @todo
+
+# Load external modules
+# ////////////////////////////////////////////////////////////
+
+# Common functions for all platforms
+. ([System.IO.Path]::Combine(
+	$script_dir,
+	'conf',
+	'common-pwsh-conf.ps1'
+))
