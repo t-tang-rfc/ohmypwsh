778c4dd68b4157cf8667970470aa45c682def5a9
tianhantang <tianhantang.pd@gmail.com>
2025-01-18 16:35:28 +0900

    Add parameter for shared storage protocol specification
    currently only supports smbfs

--- conf/device-00-pwsh-conf.ps1
+++ conf/device-00-pwsh-conf.ps1
@@ -3,18 +3,19 @@
 # - `UrlEncode` is used to encode the volume name to handle no-ASCII characters (e.g. Japanese)
 # - This function utilizes the macOS built-in (BSD) `mount`, make sure the path is properly set
 # @todo:
-# - [ ] SMB specific
+# - [x] SMB specific
 # - [ ] Supress the output of New-Item
 # - [x] Use named parameters
 # @see: `man mount`
 Function Mount-Workspace {
 	param(
-		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$WkspID,   # workspace identifier
+		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$WkspID,        # workspace identifier
 		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$MountPoint,    # mount point
-		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$HostID,   # host identifier, IP or hostname
-		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$VolumeID,    # shared volume identifier
-		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$UserID,    # user identifier
-		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$UserPSW # passphrase
+		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$HostID,        # host identifier, IP or hostname
+		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$VolumeID,      # shared volume identifier
+		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$VolumeType,    # protocol of the shared volume
+		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$UserID,        # user identifier
+		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$UserPSW        # passphrase
 	)
 	if ("W4:" -eq $WkspID) { # Currently only support W4:
 		if (-not (Test-Path $MountPoint -PathType Container)) { # Abort if the mount point already exists, since it may be local workspace
@@ -22,6 +23,9 @@ Function Mount-Workspace {
 			try {
 				New-Item -ItemType Directory -Path $MountPoint -ErrorAction Stop
 				# Call system comamnd `mount`
+				if ("smbfs" -ne $VolumeType) { # Currently only support SMB protocol for shared volume mounting
+					throw "Volume type '$VolumeType' is not supported in this version."
+				}
 				mount -t smbfs $mount_target $MountPoint
 				if ($?) {
 					Write-Output "Successfully created mount point '$MountPoint' for '$WkspID'."
@@ -50,7 +54,7 @@ Function Mount-Workspace {
 # - `man umount`
 Function Remove-Workspace {
 	param(
-		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$WkspID, # workspace identifier
+		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$WkspID,      # workspace identifier
 		[Parameter(Mandatory = $true, ValueFromPipeline = $false)][string]$MountPoint   # mount point
 	)
 	if ("W4:" -eq $WkspID) { # Currently only support W4:
