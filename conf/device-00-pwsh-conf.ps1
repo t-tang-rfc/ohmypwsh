0762be9ed5b0760218b393f9d9fef92315cd7a31
tianhantang <tianhantang.pd@gmail.com>
2025-01-18 16:32:07 +0900

    Add Remove-Workspace function to form a pair w/ Mount-Workspace

--- conf/device-00-pwsh-conf.ps1
+++ conf/device-00-pwsh-conf.ps1
@@ -2,6 +2,10 @@
 # @note:
 # - `UrlEncode` is used to encode the volume name to handle no-ASCII characters (e.g. Japanese)
 # - This function utilizes the macOS built-in (BSD) `mount`, make sure the path is properly set
+# @todo:
+# - [ ] SMB specific
+# - [ ] Supress the output of New-Item
+# - [ ] Use named parameters
 # @see: `man mount`
 Function Mount-Workspace {
 	param(
@@ -14,15 +18,14 @@ Function Mount-Workspace {
 	)
 	if ("W4:" -eq $wksp_id) { # Currently only support W4:
 		if (-not (Test-Path $mnt_pt -PathType Container)) { # Abort if the mount point already exists, since it may be local workspace
-			$mount_target = "smb://${usr_id}:${passphrase}@${host_id}/$([System.Web.HttpUtility]::UrlEncode($vol_id))"
+			$mount_target = "//${usr_id}:${passphrase}@${host_id}/$([System.Web.HttpUtility]::UrlEncode($vol_id))"
 			try {
 				New-Item -ItemType Directory -Path $mnt_pt -ErrorAction Stop
-				# Call system mount comamnd
+				# Call system comamnd `mount`
 				mount -t smbfs $mount_target $mnt_pt
 				if ($?) {
-					Write-Host "Mount point '$wksp_id' is successfully created at '$mnt_pt'."
+					Write-Output "Successfully created mount point '$mnt_pt' for '$wksp_id'."
 				} else {
-					Write-Error "Failed to mount '$wksp_id' at '$mnt_pt'."
 					throw "Failed to mount '$wksp_id' at '$mnt_pt'."
 				}
 			} catch {
@@ -34,6 +37,42 @@ Function Mount-Workspace {
 			}
 		} else {
 			Write-Error "Mount point '$wksp_id' already exists! Please take care!"
+		}
+	} else {
+		Write-Error "Workspace '$wksp_id' is not supported in this version."
+	}
+}
+
+# @brief: Remove mounted workspace served by the remote shared storage
+# @note:
+# - This function utilizes the macOS built-in (BSD) `umount`, make sure the path is properly set
+# @see:
+# - `man umount`
+Function Remove-Workspace {
+	param(
+		[Parameter(Mandatory = $true, Position = 0)][string]$wksp_id, # workspace identifier
+		[Parameter(Mandatory = $true, Position = 1)][string]$mnt_pt   # mount point
+	)
+	if ("W4:" -eq $wksp_id) { # Currently only support W4:
+		if (-not (Test-Path $mnt_pt -PathType Container)) { # Abort if the mount point does not exist
+			Write-Error "The specified mount point '$mnt_pt' does not exist or is not mounted."
+			return
+		}
+		try {
+			# Call system command `umount`
+			umount $mnt_pt 2>$null
+			if (-not $?) {
+				# Fallback mechanism for force un-mounting
+				diskutil unmount force $mnt_pt
+				if (-not $?) {
+					throw "Failed to unmount '$wksp_id' from '$mnt_pt'."
+				}
+			}
+			# Clean up
+			Remove-Item -Path $mnt_pt -ErrorAction Stop
+			Write-Output "Successfully unmounted '$wksp_id' from '$mnt_pt' and cleaned up the directory."
+		} catch {
+			Write-Error $_.Exception.Message
 		}
 	} else {
 		Write-Error "Workspace '$wksp_id' is not supported in this version."
